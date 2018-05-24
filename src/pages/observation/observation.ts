45f52e714faff309d0c9d809279affe16025335c
Gary Davies <gary@gaiaresources.com.au>
2018-05-24 14:01:54 +0800

    Added observation page

---
+++ src/pages/observation/observation.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-observation',
+  templateUrl: 'observation.html'
+})
+export class ObservationPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
