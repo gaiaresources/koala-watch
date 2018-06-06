33b4b08ae7a69c2faec4c4062d9b9bfd37fd7c8a
Voon-Li CHUNG <39473358+chungvl@users.noreply.github.com>
2018-06-06 14:12:08 +0800

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
     production: false,
-    server: 'https://staging-biosys.gaiaresources.com.au/',
+    server: 'http://localhost:8000/',
     apiExtension: '/api/',
     logoutUrl: '/#/login/',
     cookieAuthToken: 'auth_token'
