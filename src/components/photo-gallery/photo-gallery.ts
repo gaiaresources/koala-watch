dc72fb049a9113dca2fd6063e314dc4b87444525
Gary Davies <gary@gaiaresources.com.au>
2018-06-14 10:42:00 +0800

    PhotoGalleryComponent, added component dependencies

--- src/components/photo-gallery/photo-gallery.ts
+++ src/components/photo-gallery/photo-gallery.ts
@@ -1,4 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Camera, CameraOptions } from "@ionic-native/camera";
+import { StorageService } from "../../shared/services/storage.service";
+import { DomSanitizer } from "@angular/platform-browser";
+import { UUID } from "angular2-uuid";
+import * as moment from 'moment/moment';
 
 @Component({
     selector: 'photo-gallery',
@@ -6,7 +11,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class PhotoGalleryComponent {
 
-    constructor() {
+    constructor(private camera: Camera, private domSanitizer: DomSanitizer, private storageService: StorageService) {
     }
 
     public takePhoto() {
