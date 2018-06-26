2d2ca99ef89611769606300d6a98543aa7d10355
Gary Davies <gary@gaiaresources.com.au>
2018-06-26 15:47:01 +0800

    Login, swapped ion-spinner for LoadingController

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AlertController, IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AlertController, IonicPage, Loading, LoadingController, NavController, NavParams } from 'ionic-angular';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../biosys-core/services/auth.service';
 import { Dataset } from '../../biosys-core/interfaces/api.interfaces';
@@ -24,11 +24,12 @@ import { ApiResponse } from '../../shared/interfaces/mobile.interfaces';
 })
 export class LoginPage {
     public form: FormGroup;
-    public showSpinner = false;
+
+    private loading?: Loading;
 
     constructor(public navCtrl: NavController, public navParams: NavParams, private apiService: APIService,
                 private authService: AuthService, private storageService: StorageService,
-                private formBuilder: FormBuilder, private alertController: AlertController) {
+                private formBuilder: FormBuilder, private alertController: AlertController, private loadingCtrl: LoadingController) {
         this.form = this.formBuilder.group({
             'username': ['', Validators.required],
             'password': ['', Validators.required]
@@ -36,18 +37,21 @@ export class LoginPage {
     }
 
     public login() {
-        this.showSpinner = true;
+        this.loading = this.loadingCtrl.create({
+            content: 'Logging in'
+        });
+        this.loading.present();
         this.authService.login(this.form.value['username'], this.form.value['password']).subscribe(() => {
                 this.apiService.getDatasets({project__name: 'NSW Koala Data Capture'}).pipe(
                     mergeMap((datasets: Dataset[]) => from(datasets).pipe(
                         mergeMap((dataset: Dataset) => this.storageService.putDataset(dataset))
                     ))
                 ).subscribe();
-                this.showSpinner = false;
+                this.loading.dismiss();
                 this.navCtrl.setRoot('HomePage');
             },
             (error) => {
-                this.showSpinner = false;
+                this.loading.dismiss();
                 const apiResponse = formatAPIError(error) as ApiResponse;
                 this.alertController.create({
                     title: 'Login Problem',
