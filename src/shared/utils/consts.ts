283f5b46bee864df33071745afdef112cf00506e
Voon-Li CHUNG <voonli@gaiaresources.com.au>
2019-01-24 12:45:37 +0800

    T&C was provided by the client...

--- src/shared/utils/consts.ts
+++ src/shared/utils/consts.ts
@@ -20,17 +20,173 @@ export let TOAST_DURATION = 3000;
 
 export let UPDATE_BUTTON_NAME = 'Use Current Location';
 
-export let SIGNUP_TERMS_AND_CONDITIONS_HTML = '<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has ' +
-  'roots in a piece of classical Latin literature from 45 BC, making it over 2000 years ' +
-  'old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked ' +
-  'up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and ' +
-  'going through the cites of the word in classical literature, discovered the undoubtable ' +
-  'source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et ' +
-  'Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a ' +
-  'treatise on the theory of ethics, very popular during the Renaissance. The first line of ' +
-  'Lorem Ipsum, Lorem ipsum dolor sit amet..., comes from a line in section 1.10.32.</p>' +
-  '\n\n\n' +
-  '<p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those ' +
-  'interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero ' +
-  'are also reproduced in their exact original form, accompanied by English versions from ' +
-  'the 1914 translation by H. Rackham.</p>';
+export let SIGNUP_TERMS_AND_CONDITIONS_HTML =
+`<h1>Privacy Policy</h1>
+<p>Last modified on 21 January 2019</p>
+<p>This is the privacy policy ('Privacy Policy') of the Office of Environment and Heritage’s
+BioNet website located at www.bionet.nsw.gov.au (the Website), and Biosys, BioNet-Atlas,
+  BioNet Vegetation Classification, BioNet APIs and I-Spy-Koala applications (collectively the
+'App'). This Privacy Policy describes how OEH ('BioNet', 'Biosys', 'I Spy Koala', 'We', 'Us', 'Our') handles personal information (as defined below) that users provide to Us, or that We collect from You though the use of Our Websites or Apps.</p>
+
+<h2>Web site and App Visitors</h2>
+<p>The OEH collects non-personally-identifying information of the sort that web
+browsers and servers typically make available, such as the browser type, language preference,
+referring site, and the date and time of each visitor request. OEH’s purpose in collecting
+non-personally identifying information is to better understand how OEH visitors use its Website
+and Apps. From time to time, OEH may release non-personally-identifying information in the
+aggregate, e.g. by publishing a report on trends in the usage of its Website and Apps.</p>
+
+<h2>Personal Information</h2>
+<p>'Personal Information' means information such as Your first / middle initial or name,
+last name, e-mail address, street address, mailing address if different, town or city, state,
+postal code, telephone number, date of birth, location data, Internet Protocol (IP) addresses,
+personal photo, and any other information that would allow someone to identify You or contact
+You.</p>
+
+<p>OEH does not disclose such information other than as specified below.</p>
+
+<h2>The Personal Information We Collect</h2>
+<p>OEH may collect Personal Information when You choose to interact with the App in ways
+that require OEH to gather such information, such as if You browse on www.bionet.nsw.gov.au,
+register as an App User, update or add information in the App, provide content to BioNet through
+Your computer or phone, or otherwise communicate with Us about the OEH service. When You post an
+Observation on the App, We may collect Your User ID, latitude and longitude of the site of the
+Observation, the place name of the site of the Observation, the date and time of the Observation,
+the app You used to contribute data, and the time zone of the site of the Observation. The amount
+and type of information that OEH gathers depends on the nature of the interaction. In each case,
+OEH collects such information only insofar as is necessary or appropriate to fulfil the purpose
+of Your interaction with OEH. OEH does not disclose personally-identifying information other than
+as described below, and You can always refuse to supply personally-identifying information, with
+the caveat that doing so may prevent You from engaging in certain Website and App-related
+activities.</p>
+
+<h2>How We Use Personal Information</h2>
+<p>We may use the Personal Information that You provide in order to provide the OEH services
+and to administer and maintain the Website. If You register as an OEH User, You must give Us
+current, complete, and accurate information and keep the information You provide to Us up to
+date. We cannot and shall not be responsible for any problems or liability that may arise if
+You do not give Us current, accurate, truthful or complete information or You fail to update
+the information You give Us.</p>
+
+<h2>How We Disclose and Share Personal Information</h2>
+<p>Personal Information related to the Observations You post (User ID, date, time, location) 
+is published on BioNet as matter of normal usage and therefore is shared with other BioNet Users. 
+We explicitly share this public information in a machine-readable format for certain Observations 
+with a handful of partners, including the Atlas of Living Australia, the Australian Ecological 
+Knowledge Observation System (AEKOS), the Sharing and Enabling Environmental Data (SEED), and 
+the Global Biodiversity Information Facility (“GBIF”).</p>
+
+<p>Personal Information associated with Users’ registration and account (name, email 
+address, etc.) is shared with OEH’s BioNet staff.  OEH discloses potentially 
+personally-identifying and personally-identifying information only to those of its programmers, 
+contractors, and affiliated organizations that (i) need to know that information in order to 
+process it on OEH’s behalf or to provide services available at OEH’s Website and Apps, 
+and (ii) that have agreed not to disclose it to others.</p>
+
+<p>We share IP addresses, User agents, and information relating to software stability
+(e.g. information about software crashes) and behaviour (e.g. the sequence of screens visited) 
+with several analytics services. These processors include Google Analytics, and New Relic.
+We do not share Your User ID, Username, email address, or name with any of these services.</p>
+
+<p>Other than to its employees and contractors, as described above, OEH discloses 
+potentially personally-identifying and personally-identifying information only when 
+required to do so by law, when OEH believes it is necessary to do so in the interest of 
+User safety, or when OEH believes in good faith that disclosure is reasonably necessary to 
+protect the property or rights of OEH, third parties, or the public at large. Unless authorized 
+by You through Your opt-in, or as provided above, We will make Your Personal Information 
+available to non-affiliated third parties only in the following circumstances:</p>
+
+<ul>
+<li>If We are compelled to do so by a governmental agency, court, or other entity 
+(e.g. to respond to subpoenas, court orders, or legal process);</li>
+<li>If We believe Your actions violate a law, regulation, this Privacy Policy, or any 
+applicable Website and App(s) Terms of Service, or if You threaten the rights, property or 
+safety of Us, Our Website, Our Apps, any other Users, or third party;</li>
+<li>If, in Our sole discretion, We believe disclosure is necessary to investigate or 
+resolve possible problems or inquiries, to protect Our assets, to defend Our interests, 
+or comply with Our legal and regulatory obligations; or</li>
+<li>If there is a merger, transfer of control, joint venture or other business combination 
+involving OEH.</li>
+</ul>
+
+<p>If You are a registered User of OEH Website or Apps and have supplied Your email address, 
+OEH may occasionally send You an email to tell You about new features, solicit Your feedback, or 
+keep You up to date with the OEH Website and App.  If You send Us a request (for example via a 
+support email or via one of Our feedback mechanisms), We reserve the right to publish it in 
+order to help Us clarify or respond to Your request or to help Us support other Users. OEH takes 
+all measures reasonably necessary to protect against the unauthorized access, use, alteration, 
+or destruction of potentially personally-identifying and personally-identifying information.</p>
+  
+<p>We do not share Your Personal Information with any parties for commercial purposes.</p>
+
+<h2>How We Retain Personal Information</h2>
+<p>We retain Your personal information associated with Your registration for so long as You 
+remain a registered User of an OEH Website or App.  In addition, We may retain personal 
+information from closed accounts to comply with national laws, prevent fraud, resolve disputes, 
+troubleshoot problems, assist with any investigation, enforce Our Terms of Service and take other 
+actions permitted or required by applicable national laws.</p>
+
+<p>Some Personal Information associated with Observations (User_ID, time, date, location, etc.) 
+may remain attached to Observations, even after You have closed Your account.</p>
+
+<h2>Children</h2>
+<p>We do not knowingly collect or solicit personal information from children or knowingly 
+allow children to purchase goods or memberships through Our Website and Apps. Visitors 16 
+years of age and younger must obtain a parent or guardian's permission before submitting any 
+personal information to this or any other website or App. In the event that We learn that We 
+have received any personal information from a visitor 12 years of age or younger, and We do 
+not receive parental permission within a reasonable period of time of Our request for it, We will 
+delete that information as quickly as possible. If You believe that We might have information from 
+or about a child 12 years of age or younger, please contact Us at bionet@environment.nsw.gov.au</p>
+
+<h2>Cookies</h2>
+<p>A cookie is a string of information that a website stores on a visitor's computer, 
+and that the visitor's browser provides to the website each time the visitor returns. OEH may 
+uses cookies to help OEH identify and track visitors, their usage of the OEH Website, and their 
+Website access preferences. OEH visitors who do not wish to have cookies placed on their 
+computers should set their browsers to refuse cookies before using OEH websites, with the 
+drawback that certain features of OEH websites may not function properly without the aid of 
+cookies.</p>
+  
+<h2>How We Protect Your Personal Information</h2>
+<p>We are committed to protecting the Personal Information You share with Us through use of 
+OEH website and Apps, and We maintain reasonable physical, electronic, and procedural safeguards 
+to protect Your Personal Information. We limit access to OEH Websites and Apps by Our own 
+employees to individuals who are authorized for the proper handling of such information and any 
+employee found violating Our standards of security and confidentiality is subject to Our 
+disciplinary processes. We request Our service providers to follow the same policy. 
+Unfortunately, We cannot guarantee that data transmitted over the Internet will always be 
+secure. As a result, although We strive to protect Your Personal Information, We cannot ensure 
+or warrant the security of any information You transmit or We may learn as a result of Your use 
+of OEH Website and App and You do so at Your own risk. If, for any reason, You do not agree 
+with this Privacy Policy, please do not use or attempt to take advantage of any of the 
+information, services, features, or functions of OEH Website and Appthat might require You to 
+provide Your Personal Information.</p>
+
+<h2>Your Rights with Your Personal Information</h2>
+<p>You have the right to know what Personal Information We have about You, request a copy of it, 
+update and correct Your Personal Information, request that We stop collecting Your Personal 
+Information, request a transfer of Your Personal Information, or ask questions about any 
+automated decision-making processes regarding Your Personal Information. For any such 
+information or requests, contact Our Data Protection Officer at bionet@environment.nsw.gov.au</p>
+
+<h2>Posting Observations</h2>
+<p>If You do not wish to have the time, date, place, or other identifying information of 
+an Observation included on OEH Websites and Apps, You should not post an Observation. Once 
+You post an Observation, You may not be able to change or remove it. Upon Your request we 
+will remove Your Observations.</p>
+
+<h2>Withdraw Consent</h2>
+<P>If You withdraw Your consent for the use or disclosure of Your Personal Information 
+for purposes set out in this Privacy Policy You may not have access to all Our Services 
+and We might not be able to provide You all of the Services and customer support offered 
+to Our Users and authorized under this Privacy Policy and Our Terms of Service Agreement.</P>
+
+<h2>Privacy Policy Changes</h2>
+<p>Although most changes are likely to be minor, iNaturalist.org may change its Privacy 
+Policy from time to time, and in OEH sole discretion. We will post an updated and revised 
+version of this Privacy Policy on the BioNet Website when any material changes have been made, 
+and OEH encourages visitors to check this Privacy Policy often so You are aware of the most 
+current terms and conditions that apply to You. The revisions are effective immediately upon 
+posting. Your continued use of this site after any change in this Privacy Policy will 
+constitute Your acceptance of such change.</p>`;
