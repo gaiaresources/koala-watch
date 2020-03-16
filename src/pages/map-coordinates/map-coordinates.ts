31d9bd8418c6c29bc000763d8819fcb4eaa9c959
Jason Grzywna <jason.grzywna@gaiaresources.com.au>
2020-03-16 09:30:31 +0800

    BIOSYS-413 - unbroke view - map pin not working

--- src/pages/map-coordinates/map-coordinates.ts
+++ src/pages/map-coordinates/map-coordinates.ts
@@ -1,114 +1,85 @@
-import { GoogleMap, GoogleMaps, LatLng, Marker, } from '@ionic-native/google-maps';
-import { Component, Input, OnInit } from '@angular/core/';
-import { ClientRecord } from '../../shared/interfaces/mobile.interfaces';
-import { Events, NavParams } from 'ionic-angular';
-import { timer } from 'rxjs/observable/timer';
-import { isDatasetCensus } from '../../shared/utils/functions';
-import * as moment from 'moment/moment';
+import { Component, OnInit } from '@angular/core';
+import { Events, IonicPage, NavController, NavParams } from 'ionic-angular';
+import { GoogleMap, GoogleMaps, ILatLng, LatLng, Marker } from '@ionic-native/google-maps';
+import { timer } from '../../../node_modules/rxjs/observable/timer';
 
-@Component({
-    selector: 'records-map',
-    templateUrl: 'records-map.html'
+/**
+ * Generated class for the MapCoordinatesPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage({
+  name: 'mcp',
+  segment: 'mcp'
 })
-export class RecordsMapComponent implements OnInit {
-    @Input()
-    public set records(records: ClientRecord[]) {
-        this._records = records;
-    }
+@Component({
+  selector: 'page-map-coordinates',
+  templateUrl: 'map-coordinates.html',
+})
+export class MapCoordinatesPage implements OnInit {
+  private map: GoogleMap;
+  private dragMarker: Marker;
+  private startPos: ILatLng;
 
-    private map: GoogleMap;
-    private _records: ClientRecord[];
-    private dragMarker: Marker;
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              private events: Events) {
+    this.startPos = this.navParams.data;
+    console.log('navpar', this.navParams);
+    console.log('startpos', this.startPos);
+    return;
+  }
 
-    constructor(private navParams: NavParams,
-                private events: Events) {
-    }
+  ionViewDidLoad() {
+  }
 
-    ngOnInit(): void {
-        this.map = GoogleMaps.create('map');
-        this.map.setOptions({
-            'backgroundColor': 'white',
-            'building': false,
-            'mapType': 'MAP_TYPE_HYBRID',
-            'controls': {
-                'compass': false,
-                'zoom': false,
-                'indoorPicker': false,
-            },
-            'gestures': {
-                'scroll': true,
-                'zoom': true,
-                'tilt': false,
-                'rotate': false,
-            },
-            'camera': {
-                'target': new LatLng(-25, 132),
-                'zoom': 3.5,
-            }
-        });
-        this.map.setMyLocationEnabled(true);
-        this.map.setMyLocationButtonEnabled(true);
-        if (this.navParams.data.hasOwnProperty('data')) {
-            this.records = this.navParams.get('data');
-        }
-        this.events.subscribe('home-willenter', () => this.ionViewWillEnter());
-        this.events.subscribe('map-whereispin', () => this.dragMarkerLocation());
-    }
+  ngOnInit(): void {
+    this.map = GoogleMaps.create('map-getpin');
+    this.map.setOptions({
+      'backgroundColor': 'white',
+      'building': false,
+      'mapType': 'MAP_TYPE_HYBRID',
+      'controls': {
+        'compass': false,
+        'zoom': false,
+        'indoorPicker': false,
+      },
+      'gestures': {
+        'scroll': true,
+        'zoom': true,
+        'tilt': false,
+        'rotate': false,
+      },
+      'camera': {
+        'target': new LatLng(-25, 132),
+        'zoom': 3.5,
+      }
+    });
+    this.map.setMyLocationEnabled(true);
+    this.map.setMyLocationButtonEnabled(true);
+    this.events.subscribe('home-willenter', () => {});
+    this.events.subscribe('map-whereispin', () => {});
+    timer(100).subscribe(() => {
+      let position: LatLng = new LatLng(this.startPos.lat, this.startPos.lng);
+      if (!(Math.abs(position.lat) > 0.1 && Math.abs(position.lng) > 0.1)) {
+        position = new LatLng(-33.0, 146.012);
+      }
+      console.log('pos', position);
+      const options = {
+        snippet: 'Move this pin to the koala sighting location',
+        title: 'Move this pin to the koala sighting location',
+        position: position,
+        draggable: true
+      };
+      this.dragMarker = this.map.addMarkerSync(options);
+    });
+  }
 
-    public ionViewWillEnter() {
-        timer(500).subscribe(() => this.loadMarkers());
-    }
-
-    private dragMarkerLocation() {
-      console.log('PositionMap', this.dragMarker.getPosition())
-      this.events.publish('map-specifiedcoordinates', this.dragMarker.getPosition());
-    }
-
-    private loadMarkers() {
-        if (this.map) {
-            this.map.clear();
-            if (this._records && this._records.length) {
-                for (const record of this._records) {
-                    if (record.hasOwnProperty('data') &&
-                        record.data.hasOwnProperty('Latitude') &&
-                        record.data.hasOwnProperty('Longitude')) {
-                        const title = record.datasetName;
-                        const snippet = moment(record.datetime).format('DD/MM/YYYY HH:mm');
-                        let url = 'assets/imgs/';
-                        url += `${isDatasetCensus(record.datasetName) ? 'tree' : 'eye'}-pin-`;
-                        url += `${record.valid ? 'complete' : 'incomplete'}.png`;
-
-                        const marker = this.map.addMarkerSync({
-                            snippet: snippet,
-                            title: title,
-                            icon: {
-                                url: url,
-                                size: {
-                                    width: 45,
-                                    height: 45
-                                }
-                            },
-                            animation: 'DROP',
-                            position: {
-                                lat: record.data.Latitude,
-                                lng: record.data.Longitude,
-                            }
-                        });
-                        // FIXME: work out why selector toggles slow to a crawl
-                        // marker.addEventListener(GoogleMapsEvent.MARKER_CLICK).subscribe(
-                        //     (value) => {
-                        //         const page = record.datasetName.toLowerCase().indexOf('census') > -1 ? 'CensusPage' : 'ObservationPage';
-                        //         const params = {
-                        //             datasetName: record.datasetName,
-                        //             recordClientId: record.client_id,
-                        //             parentId: record.parentId
-                        //         };
-                        //         this.navParams.get('navCtrl').push(page, params);
-                        //         return;
-                        //     });
-                    }
-                }
-            }
-        }
-    }
+  useClicked() {
+    this.events.publish('map-returnCoordinates', this.dragMarker.getPosition());
+    this.navCtrl.pop();
+    return;
+  }
 }
