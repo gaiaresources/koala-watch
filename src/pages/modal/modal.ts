b5e08b17ae593f8088d0a2c359f091b9e7dc87d9
Jason <jason@gaiaresources.com.au>
2020-03-17 17:04:11 +0800

    BIOSYS-413 works in modal - BUG: doesnt update lat long vals

--- src/pages/modal/modal.ts
+++ src/pages/modal/modal.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController, Platform, Events } from 'ionic-angular';
+import { GoogleMap, GoogleMaps, ILatLng, LatLng, Marker, GoogleMapOptions } from '@ionic-native/google-maps';
+import { timer } from 'rxjs/observable/timer';
 
 /**
  * Generated class for the ModalPage page.
@@ -14,18 +16,82 @@ import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angul
   templateUrl: 'modal.html',
 })
 export class ModalPage {
+  private map: GoogleMap;
+  private dragMarker: Marker;
+  private startPos: ILatLng;
 
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
-    public viewCtrl: ViewController) {
+    public viewCtrl: ViewController,
+    private events: Events,
+    private platform: Platform) {
+      this.startPos = this.navParams.data;
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ModalPage');
-    console.log(this.navParams.get('message'));
+    this.platform.ready().then(() => {
+      this.loadMap();
+    });
   }
 
-  public closeModal(){
+  loadMap(): void {
+    this.map = GoogleMaps.create('map-getpin');
+    this.map.setOptions({
+      'backgroundColor': 'white',
+      'building': false,
+      'mapType': 'MAP_TYPE_HYBRID',
+      'controls': {
+        'compass': false,
+        'zoom': false,
+        'indoorPicker': false,
+      },
+      'gestures': {
+        'scroll': true,
+        'zoom': true,
+        'tilt': false,
+        'rotate': false,
+      },
+      'camera': {
+        'target': new LatLng(-25, 132),
+        'zoom': 3.5,
+      }
+    });
+    this.map.setMyLocationEnabled(true);
+    this.map.setMyLocationButtonEnabled(true);
+    this.events.subscribe('home-willenter', () => { });
+    this.events.subscribe('map-whereispin', () => { });
+    timer(100).subscribe(() => {
+      let position: LatLng = new LatLng(this.startPos.lat, this.startPos.lng);
+      if (!(Math.abs(position.lat) > 0.1 && Math.abs(position.lng) > 0.1)) {
+        position = new LatLng(-33.0, 146.012);
+      }
+      const options = {
+        snippet: 'Move this pin to the koala sighting location',
+        title: 'Move this pin to the koala sighting location',
+        position: position,
+        draggable: true
+      };
+      this.dragMarker = this.map.addMarkerSync(options);
+    });
+  }
+
+  useClicked() {
+    this.events.publish('map-returnCoordinates', this.dragMarker.getPosition());
+
+    this.closeModal();
+  }
+
+  // hack required to remove maps background
+  private cleanup() {
+    const nodeList = document.querySelectorAll('._gmaps_cdv_');
+
+    for (let k = 0; k < nodeList.length; ++k) {
+        nodeList.item(k).classList.remove('_gmaps_cdv_');
+    }
+  }
+
+  public closeModal() {
+    this.cleanup();
     this.viewCtrl.dismiss();
   }
 
