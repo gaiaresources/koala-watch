d1d80d250fa0324a5a1aef405461172851460548
Jason Grzywna <jason.grzywna@gaiaresources.com.au>
2021-07-12 10:33:47 +0800

    BIOSYS-434 - simplify android build script

--- android-build-prod.sh
+++ android-build-prod.sh
@@ -1,4 +1,10 @@
 cross-env ENV='prod' ionic cordova build --release android
-~/Android/Sdk/build-tools/29.0.2/zipalign -v -p 4 platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk $$.1.apk
-~/Android/Sdk/build-tools/29.0.2/apksigner sign --ks my-release-key.jks --out `date +"%Y%m%d-%H%M%S"`.apk $$.1.apk
+
+# change this to your own android sdk path
+
+# linux: ANDROID_SDK_PATH=~/Android/Sdk/build-tools
+ANDROID_SDK_PATH=~/Library/Android/sdk/build-tools
+
+$ANDROID_SDK_PATH/29.0.2/zipalign -v -p 4 platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk $$.1.apk
+$ANDROID_SDK_PATH/29.0.2/apksigner sign --ks my-release-key.jks --out `date +"%Y%m%d-%H%M%S"`.apk $$.1.apk
 rm $$.1.apk
