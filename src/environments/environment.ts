c1065d8e828a455d7187015a34896f289f9ca692
Jason Grzywna <jason.grzywna@gaiaresources.com.au>
2022-01-27 14:35:41 +0800

    BIOSYS-437 - text

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,3 +10,12 @@ export const environment = {
   logoutUrl: '/#/login/',
   cookieAuthToken: 'auth_token'
 };
+
+// enable for debug
+/* export const environment = {
+  production: false,
+  server: 'https://uat-koalawatch-api.gaiaresources.com.au',
+  apiExtension: '/api/',
+  logoutUrl: '/#/login/',
+  cookieAuthToken: 'auth_token'
+}; */
